6802fb8f2217d73a466a7bb1a1a673daa872d6fd
felunka <felixvontiedemann@gmail.com>
2026-01-02 19:03:46 +0100

    Fix root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root "sessions#new"
+  root "nodes#index"
 
   resource :session, only: [ :new, :create, :destroy ]
   resources :nodes do
