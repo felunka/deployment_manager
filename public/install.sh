252d1162fa01d0886a520ab5f1414dde1ff84a23
felunka <felixvontiedemann@gmail.com>
2026-01-02 14:54:48 +0100

    Revert "Update install script to hard code docker release"
    This reverts commit 097f8c4a4bb58a2869525c1b9e249a69c917bcf7.

--- public/install.sh
+++ public/install.sh
@@ -29,7 +29,7 @@ apt-get install -y --no-install-recommends \
 if ! command -v docker >/dev/null 2>&1; then
   apt-get remove -y docker docker-engine docker.io containerd runc || true
   curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") noble stable" > /etc/apt/sources.list.d/docker.list
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
   apt-get update
   apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io docker-compose-plugin
 fi
