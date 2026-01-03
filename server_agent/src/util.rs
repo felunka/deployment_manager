4995f3de35873a811291afa4daebfce6841b1e50
felunka <felixvontiedemann@gmail.com>
2026-01-03 23:21:24 +0100

    Add debug oout to command runs

--- server_agent/src/util.rs
+++ server_agent/src/util.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
-pub fn command_output(command: &str, args: Option<Vec<&str>>, current_dir: Option<&str>) -> String {
-    let mut command = Command::new(command);
+pub fn command_output(command_str: &str, args: Option<Vec<&str>>, current_dir: Option<&str>) -> String {
+    let mut command = Command::new(command_str);
     if args.is_some() {
         for arg in args.unwrap() {
             command.arg(arg);
@@ -11,5 +11,12 @@ pub fn command_output(command: &str, args: Option<Vec<&str>>, current_dir: Optio
         command.current_dir(current_dir.unwrap());
     }
     let output = command.output().expect("Failed to execute command");
-    String::from_utf8_lossy(&output.stdout).to_string()
+    let stdout_str = String::from_utf8_lossy(&output.stdout).to_string();
+    let stderr_str = String::from_utf8_lossy(&output.stderr).to_string();
+
+    println!("{}", command_str);
+    println!("{}", stdout_str);
+    println!("{}", stderr_str);
+    
+    stdout_str
 }
